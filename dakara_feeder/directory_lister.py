1720d3a6ac1f286879a7311d560e670456ad1ee8
Neraste <neraste.herr10@gmail.com>
2019-07-07 18:31:54 +0900

    Make directory lister more polyvalent
    WIP, not tested.

--- dakara_feeder/directory_lister.py
+++ dakara_feeder/directory_lister.py
@@ -1,10 +1,12 @@
 import logging
+from itertools import groupby
 
 
 logger = logging.getLogger(__name__)
 
 
 VIDEO_EXTENSIONS = [".avi", ".mkv", ".mp4", ".mpeg", ".mpg", ".vob", ".webm"]
+SUBTITLE_EXTENSIONS = []
 
 
 def list_directory(path):
@@ -20,9 +22,45 @@ def list_directory(path):
             each file is relative to the given path.
     """
     logger.debug("Listing %s", path)
-    content = [
-        p.relpath(path) for p in path.walkfiles() if p.ext.lower() in VIDEO_EXTENSIONS
-    ]
-    logger.debug("Listed %i files", len(content))
+    files_list = path.walkfiles()
+    logger.debug("Listed %i files", len(files_list))
 
-    return content
+    return [
+        item
+        for _, files in groupby(files_list, lambda f: f.stem)
+        for item in group_by_type(files)
+    ]
+
+def group_by_type(files):
+    # sort files by their extension
+    videos = []
+    subtitles = []
+    others = []
+    for file in files:
+        if file.ext in VIDEO_EXTENSIONS:
+            videos.append(file)
+            continue
+
+        if file.ext in SUBTITLE_EXTENSIONS:
+            subtitles.append(file)
+            continue
+
+        others.append(file)
+
+    # check there is at least one video
+    if len(videos) == 0:
+        return []
+
+    # check there if there are only one subtitle
+    if len(subtitles) > 1:
+        logger.warning("More than one subtitle for video %s", videos[0])
+        return []
+
+    # recombine the files
+    return [
+        {
+            "video": video,
+            "subtitle": subtitles[0] if subtitles else None,
+            "others": others
+        } for video in videos
+    ]
