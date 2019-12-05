d740b00d44e24d34f0ec51011215b03412b9983d
Neraste <neraste.herr10@gmail.com>
2019-12-06 00:32:22 +0900

    Version 1.5.0

--- src/dakara_feeder/version.py
+++ src/dakara_feeder/version.py
@@ -4,7 +4,7 @@ from pkg_resources import parse_version
 import importlib_metadata
 
 __version__ = importlib_metadata.version("dakarafeeder")
-__date__ = "1970-01-01"
+__date__ = "2019-12-05"
 
 logger = logging.getLogger(__name__)
 
